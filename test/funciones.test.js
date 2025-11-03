b2d5fc81de742fab0bec89b420b53f15485868fb
keynis <keydejun@gmail.com>
2025-11-03 20:11:51 +0100

    Test para la funcion de Fibonacci si n es menos igual a 0

---
+++ test/funciones.test.js
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+import { fibonacci } from '../src/funciones.js'
+
+describe('Función fibonacci(n)', () => {
+    it('Debe lanzar RangeError si n ≤ 0', () => {
+      expect(() => fibonacci(0)).toThrowError(new RangeError('solo números enteros ≥ 0'))
+      expect(() => fibonacci(-3)).toThrowError(new RangeError('solo números enteros ≥ 0'))
+    })
+})
