2431efe620e391bc990f32f39d3a1fe93e499e1b
keynis <keydejun@gmail.com>
2025-11-03 20:14:27 +0100

    Test para la funcion de Fibonacci, n es mayor que 2

--- test/funciones.test.js
+++ test/funciones.test.js
@@ -14,4 +14,12 @@ describe('Función fibonacci(n)', () => {
       it('Debe devolver 1 si n = 1', () => {
         expect(fibonacci(1)).toBe(1)
       })
+
+      it('Debe devolver la suma de los dos anteriores para n ≥ 2', () => {
+        expect(fibonacci(2)).toBe(1)
+        expect(fibonacci(3)).toBe(2)
+        expect(fibonacci(4)).toBe(3)
+        expect(fibonacci(5)).toBe(5)
+        expect(fibonacci(6)).toBe(8)
+      })
 })
